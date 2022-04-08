7551f3f6b54bf11d8a8a582b2ce24de7043d8b7f
Bernas <bernasfixolas@hotmail.com>
2022-04-08 15:04:27 +0100

    Added code for coloring correct or incorrect typing

--- views/gamescreen.py
+++ views/gamescreen.py
@@ -11,7 +11,7 @@ class Gamescreen(CustomWindow):
     timerLbl = 0
     startButton = 0
     timer = 60
-
+    currentIndex = 0
     def __init__(self, root):
         super().__init__(root)
 
@@ -20,7 +20,7 @@ class Gamescreen(CustomWindow):
         self.window.grid_columnconfigure((0, 4), weight=1)   
 
         # Text area config
-        container = Frame(self.window, borderwidth=1, relief="sunken", width=600, height=250)
+        container = Frame(self.window, borderwidth=1, relief="sunken", width=600, height=350)
         container.grid_propagate(False)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
@@ -31,9 +31,14 @@ class Gamescreen(CustomWindow):
         self.textArea.insert(INSERT,' '.join(WordList.wordlist))
         self.textArea.grid(row=0, column=0, sticky="nsew")
         self.textArea.config(state='disabled')
+
+        for i in range(0, len(self.textArea.get("1.0",END)) + 1):
+            self.textArea.tag_add(f'{i}',f'1.{i}')
         
         #textfield config
-        self.textfield = Entry(self.window, width=50)
+        sv = StringVar()
+        sv.trace("w", lambda name, index,mode, sv=sv: self.onType(sv))
+        self.textfield = Entry(self.window, width=50, textvariable = sv)
         self.textfield.grid(row=2, column=1, pady=(0,15))
         self.textfield.config(state= "disabled")
 
@@ -43,6 +48,28 @@ class Gamescreen(CustomWindow):
         self.startButton = Button(self.window, text='Start Test', command=self.onStart)
         self.startButton.grid(row=3, column=1, pady=10)
         Button(self.window, text='Return', command=self.onReturn).grid(row=4, column=1, pady=10)
+
+    # this is where the main logic for the game happens
+    def onType(self, sv):
+        #if we're at 0 chars, don't do anything
+        if len(sv.get()) == 0:
+            return
+        # handle backspaces
+        entrylength = len(sv.get())-1 
+        if(self.currentIndex < entrylength + 1):
+            self.currentIndex = entrylength + 1
+            pass
+        else:
+            self.textArea.tag_config(f"{entrylength+1}",foreground="black")
+            self.currentIndex = entrylength + 1
+            return
+
+        #color stuff correctly
+        if sv.get()[-1] == self.textArea.get(f'1.{entrylength}'):
+            self.textArea.tag_config(f"{entrylength}",foreground="green")
+        else:
+            self.textArea.tag_config(f"{entrylength}",foreground="red")
+        
 
     def onReturn(self): 
         self.quitWindow()
